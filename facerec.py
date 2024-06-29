514266c773a35b55755c84d46a814e0a507da1ab
07Roshi <roshithshetty7@gmail.com>
2024-06-29 11:47:55 +0530

    workshop

--- facerec.py
+++ facerec.py
@@ -1,47 +1,3 @@
-# import cv2 as cv
-# import argparse
-
-# def visualize(image,face,thickness = 2):
-#     for idx, face in enumerate(face[1]):
-#         coords = face[:1].astype(np.int32)
-#         cv.Rect(image,(coords[0],coords[1]),(coords[0]+coords[2],coords[1]+coords[3]),(0,255,0),thickness)
-#         cv.circle(image,(coords[4],coords[5]),2,(255,0,0),thickness)
-#         cv.circle(image,(coords[6],coords[7]),2,(0,0,255),thickness)
-#         cv.circle(image,(coords[8],coords[9]),2,(0,255,0),thickness)
-#         cv.circle(image,(coords[10],coords[11]),2,(255,0,255),thickness)
-#         cv.circle(image,(coords[12],coords[13]),2,(0,255,255),thickness)
-
-       
-
-
-# ap = argparse.ArgumentParser()
-# ap.add_argument("-r", "--reference_image", required=True,help="path to input Aadhaar reference image")
-
-# ap.add_argument("-q", "--query_image", required=True,help="path to input query image")
-# args = vars(ap.parse_args())
-
-
-# ref_image = cv.imread(args["reference_image"])
-# query_image = cv.imread(args["query_image"])
-
-# # cv.imshow("r", ref_image)
-# # cv.waitKey(0)
-# # cv.imshow("q", query_image)
-# # cv.waitKey(0)
-
-
-
-# faceDetector = cv.FaceDetectorYN.create("face_detection_yunet_2022mar.onnx", "", (ref_image.shape[1], ref_image.shape[0]), 0.9, 0.3, 5000)
-
-# faceInAadhaar = faceDetector.detect(ref_image)
-# visualize(ref_image,faceInAadhaar)
-
-# cv.imshow("face", ref_image)
-# cv.waitKey(0)
-
-# ================================================================================
-
-
 import cv2 as cv
 import numpy as np
 import argparse
@@ -122,3 +78,8 @@ msg = 'different identities'
 if l2_score > l2_similarity_threshold:
     msg = 'the same identity'
 print('They have {}. NormL2 Distance: {}, threhold: {} (lower value means higher similarity, min 0.0).'.format(msg, l2_score, l2_similarity_threshold))
+
+
+
+
+# To run : python facerec.py --reference_image .\reference_image.jpg --query_image .\query_image.jpg
